4f5d240e31d196e759437efaa44401843f0e7294
JaiderSanchez <jaiderfabiansd@gmail.com>
2024-11-04 17:07:01 -0500

    Avance del Proyecto-Práctica-Laravel sobre las rutas en Laravel.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,28 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome to the homepage";
 });
+
+Route::get('/posts', function() {
+    return "Aquí se mostrarán todos los posts";
+});
+
+Route::get('/posts/create', function() {
+    return "Aquí se mostrará un formulario para crear un post";
+});
+
+Route::get('/posts/{post}', function($post){
+    return "Este es el post {$post}";
+});
+
+
+// Route::get('/posts/{post}/{category}', function($post, $category){
+//     return "Este es el post {$post} de la categoría {$category}";
+// });
+
+//Get
+//Post
+//Put
+//Patch
+//Delete
